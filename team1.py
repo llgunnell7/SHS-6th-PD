fdf09aea513597042e691a37aca5de68d2f4b04a
llgunnell7 <llgunnell7@gmail.com>
2018-01-25 13:55:30 -0600

    changed strategy name
    changed strategy name

--- team1.py
+++ team1.py
@@ -7,7 +7,7 @@
 ####
 
 team_name = 'Team 1' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
+strategy_name = 'best strategy'
 strategy_description = 'How does this strategy decide?'
     
 def move(my_history, their_history, my_score, their_score):
