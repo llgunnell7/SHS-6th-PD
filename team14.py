0b98f7f6d7a49a9c79bd7c2dee2308733c29cd81
Mr. Mainhardt <rmainhar@shakopee.k12.mn.us>
2018-02-05 13:59:18 -0600

    Changed Strategy name

--- team14.py
+++ team14.py
@@ -7,8 +7,8 @@
 ####
 
 team_name = 'Dominator' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
+strategy_name = 'Beat all the Examples'
+strategy_description = 'The Domination Strategy'
     
 def move(my_history, their_history, my_score, their_score):
     ''' Arguments accepted: my_history, their_history are strings.
